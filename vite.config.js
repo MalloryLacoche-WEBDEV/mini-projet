b1538ea696ae331fab5491f7ce52f87426db3ad1
bubonn <mallory.lacoche@gmail.com>
2026-02-26 09:27:52 +0100

    modif vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Ce fichier configure Vite pour utiliser le plugin React.
-// Vous n'avez rien à modifier ici.
-// defineConfig() prend un objet de configuration.
-// La propriété `plugins` reçoit un tableau — ajoutez-y react() pour activer la compilation JSX.
 
 export default defineConfig({
     plugins: [react()],
 })
+
